3f0df344f42ea7e2d33967e4499d098df92e85ca
maanethdesilva <maaneth.2002@gmail.com>
2022-09-06 15:22:47 -0400

    Imported notification page

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,13 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import NotificationPage from './Notifications/NotificationPage';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NotificationPage />
   </React.StrictMode>
 );
 

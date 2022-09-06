edcedc3b717b287f68fc22d446bae851e829c103
maanethdesilva <maaneth.2002@gmail.com>
2022-09-06 18:29:13 -0400

    Fixed directory organization

--- client/src/Notifications/NotificationItem.js
+++ client/src/Notifications/NotificationItem.js
@@ -14,8 +14,10 @@ function NotificationItem(Props) {
   return (
   <div>
     <li className="list-group-item d-flex justify-content-between align-items-center">
-      {from + ": " + val.MessageContext}
       <div>
+        <b>{from + ": "}</b>{val.MessageContext}
+      </div>
+      <div className="buttonList">
         <button type="button" className="btn btn-primary">Accept</button>
         <button type="button" className="btn btn-danger">Decline</button>
         <button type="button" className="btn btn-warning">Pending</button>

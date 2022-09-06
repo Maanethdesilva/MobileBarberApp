ff7af9f3496a88655b4f282cdf48153d0a6ec536
maanethdesilva <maaneth.2002@gmail.com>
2022-09-06 17:03:33 -0400

    Installed bootstrap and made notification style changes

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import NotificationPage from './Notifications/NotificationPage';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

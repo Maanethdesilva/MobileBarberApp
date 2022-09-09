1cce91b2e57209ed068306ac1d5414966d49494e
Akshen Jasikumar <akshen.jasikumar@gmail.com>
2022-09-09 14:32:54 -0400

    Re-organized components.

---
+++ client/src/Components/NotificationItem/NotificationItem.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import * as Status from "./NotificationStatus";
+function NotificationItem(Props: any) {
+  var from = "";
+
+  const { val } = Props;
+
+  if (val.DisplayName != null) {
+    from = val.DisplayName;
+  } else {
+    from = val.FirstName + val.LastName;
+  }
+
+  return (
+    <div>
+      <li className="list-group-item d-flex justify-content-between align-items-center">
+        <div>
+          <b>{from + ": "}</b>
+          {val.MessageContext}
+        </div>
+        {val.Response != null ? (
+          <div className="buttonList">
+            {val.Response === Status.DECISON_UNMADE ? (
+              <div>
+                <button type="button" className="btn btn-primary">
+                  Accept
+                </button>
+                <button type="button" className="btn btn-danger">
+                  Decline
+                </button>
+              </div>
+            ) : val.Response === Status.ACCEPTED ? (
+              <button type="button" className="btn btn-success" disabled>
+                Accepted
+              </button>
+            ) : val.Response === Status.DECLINED ? (
+              <button type="button" className="btn btn-danger" disabled>
+                Declined
+              </button>
+            ) : val.Response === Status.PENDING ? (
+              <button type="button" className="btn btn-warning" disabled>
+                Pending
+              </button>
+            ) : null}
+          </div>
+        ) : null}
+      </li>
+    </div>
+  );
+}
+
+export default NotificationItem;
